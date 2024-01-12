9b3bf72c74bb347c69f8b73c05c994e98283a5e5
chaeryun0 <chaeryun00@gmail.com>
2024-01-12 20:23:28 +0900

    feat: 페이지 전환 시 스크롤 최상단 이동 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import StayListPage from '@pages/StayListPage'
 import SearchPage from '@pages/SearchPage'
+import ScrollToTop from '@components/shared/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<StayListPage />} />
         <Route path="/search" element={<SearchPage />} />
