2cee87671bfcf43d57c7ac4ebaa18efdaa4fd4f5
chaeryun0 <chaeryun00@gmail.com>
2024-01-13 23:57:25 +0900

    chore: 데이터 로드 방식 재변경

--- src/pages/StayListPage.tsx
+++ src/pages/StayListPage.tsx
@@ -1,15 +1,46 @@
-import Title from '@components/shared/Title'
-import StayItem from '@components/stayList/StayItem'
+import { useCallback, useEffect, useRef } from 'react'
 import useStayList from '@components/stayList/hooks/useStayList'
+import StayItem from '@components/stayList/StayItem'
+import Title from '@components/shared/Title'
 
-import IconArrowRight from '@assets/icon/icon-arrowRight.svg?react'
 import classNames from 'classnames/bind'
 import styles from './StayListPage.module.scss'
 
 const cx = classNames.bind(styles)
 
 const StayListPage = () => {
-  const { stays, loadMore, showMoreButton } = useStayList()
+  const { stays, loadMore } = useStayList()
+  const observerRef = useRef<HTMLLIElement | null>(null)
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0]
+      if (target.isIntersecting && target.intersectionRatio > 0) {
+        loadMore()
+      }
+    },
+    [loadMore],
+  )
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    })
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [observerRef, handleObserver])
+
+  if (!stays) {
+    return <div>Loading</div>
+  }
+
+  const ObserverPlaceholder = () => <li ref={observerRef}></li>
 
   return (
     <main>
@@ -21,14 +52,13 @@ const StayListPage = () => {
         />
         <ul className={cx('stayItemWrap')}>
           {stays.length > 0 &&
-            stays.map((stay) => <StayItem stay={stay} key={stay.staySeq} />)}
+            stays.map((stay, index) => [
+              <StayItem stay={stay} key={stay.staySeq} />,
+              index === stays.length - 1 && (
+                <ObserverPlaceholder key={`observer-${index}`} />
+              ),
+            ])}
         </ul>
-        {showMoreButton && (
-          <button className={cx('showMoreBtn')} onClick={loadMore}>
-            <span>더보기</span>
-            <IconArrowRight width={28} height={28} fill="var(--white)" />
-          </button>
-        )}
       </div>
     </main>
   )
