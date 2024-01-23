927a8b7836b245b779bcb47f112ed2b54f8721a4
chaeryun0 <chaeryun00@gmail.com>
2024-01-24 01:01:44 +0900

    feat: 연령별 인원수 선택 및 드롭다운 기능 추가

---
+++ src/components/search/CountSelector.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import IconMinus from '@assets/icon/icon-minus.svg?react'
+import IconPlus from '@assets/icon/icon-plus.svg?react'
+import classNames from 'classnames/bind'
+import styles from './CountSelector.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface CountSelectorProps {
+  title: string
+  description: string
+  initialCount?: number
+  onChange: (count: number) => void
+}
+
+const CountSelector = ({
+  title,
+  description,
+  initialCount = 0,
+  onChange,
+}: CountSelectorProps) => {
+  const [count, setCount] = useState(initialCount)
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+
+  useEffect(() => {
+    onChange(count)
+  }, [count, onChange])
+
+  return (
+    <div className={cx('selector')}>
+      <div className={cx('selectorText')}>
+        <h3 className={cx('title')}>{title}</h3>
+        <span className={cx('description')}>{description}</span>
+      </div>
+      <div className={cx('selectorButton')}>
+        <button
+          type="button"
+          className={cx('btn')}
+          onClick={handleDecrement}
+          disabled={title === '성인' ? count === 1 : count === 0}
+        >
+          <IconMinus width={24} height={20} />
+        </button>
+        <div className={cx('count')}>
+          <span>{count}</span>
+        </div>
+        <button type="button" className={cx('btn')} onClick={handleIncrement}>
+          <IconPlus width={24} height={20} />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default CountSelector
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,6 +3,7 @@ import { format, parseISO } from 'date-fns'
 import SearchSelector from '@components/search/SearchSelector'
 import LocationSearch from '@components/search/LocationSearch'
 import DatePicker from '@components/shared/DatePicker'
+import CountSelector from '@components/search/CountSelector'
 
 import IconSearch from '@assets/icon/icon-search.svg?react'
 import classNames from 'classnames/bind'
@@ -13,9 +14,14 @@ const cx = classNames.bind(styles)
 const SearchPage = () => {
   const [isLocationDropdownOpen, setLocationDropdownOpen] = useState(false)
   const [isDateDropdownOpen, setDateDropdownOpen] = useState(false)
+  const [isGuestDropdownOpen, setGuestDropdownOpen] = useState(false)
 
   const [searchLocation, setSearchLocation] = useState('')
   const [selectedLocation, setSelectedLocation] = useState('')
+
+  const [adultCount, setAdultCount] = useState(1)
+  const [childCount, setChildCount] = useState(0)
+  const [infantCount, setInfantCount] = useState(0)
 
   const [displayedDate, setDisplayedDate] = useState('')
   const [selectedDate, setSelectedDate] = useState<{
@@ -57,14 +63,16 @@ const SearchPage = () => {
     setSearchLocation(e.target.value)
   }
 
-  const handleDropdownToggle = (dropdown: 'location' | 'date') => {
+  const handleDropdownToggle = (dropdown: 'location' | 'date' | 'guest') => {
     setLocationDropdownOpen(dropdown === 'location' ? (prev) => !prev : false)
     setDateDropdownOpen(dropdown === 'date' ? (prev) => !prev : false)
+    setGuestDropdownOpen(dropdown === 'guest' ? (prev) => !prev : false)
   }
 
   const closeDropdowns = () => {
     setLocationDropdownOpen(false)
     setDateDropdownOpen(false)
+    setGuestDropdownOpen(false)
   }
 
   const handleDateResultClear = () => {
@@ -91,6 +99,24 @@ const SearchPage = () => {
       )
       setDateDropdownOpen(false)
     }
+  }
+
+  const formatGuestCountsString = () => {
+    const parts = []
+
+    if (adultCount > 0) {
+      parts.push(`성인 ${adultCount}명`)
+    }
+
+    if (childCount > 0) {
+      parts.push(`어린이 ${childCount}명`)
+    }
+
+    if (infantCount > 0) {
+      parts.push(`유아 ${infantCount}명`)
+    }
+
+    return parts.join(', ')
   }
 
   return (
@@ -144,6 +170,40 @@ const SearchPage = () => {
                       onComplete={() => {
                         handleDatePickerComplete()
                       }}
+                    />
+                  </div>
+                </div>
+              </SearchSelector>
+              <div className={cx('division')}></div>
+              <SearchSelector
+                label="인원"
+                isOpen={isGuestDropdownOpen}
+                showCloseIcon={false}
+                selectedResult={formatGuestCountsString()}
+                onToggle={() => handleDropdownToggle('guest')}
+              >
+                <div
+                  className={cx('dropdown', 'guest')}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <div className={cx('dropdownInner')}>
+                    <CountSelector
+                      title="성인"
+                      description="13세 이상"
+                      initialCount={adultCount}
+                      onChange={(count) => setAdultCount(count)}
+                    />
+                    <CountSelector
+                      title="어린이"
+                      description="2세~12세"
+                      initialCount={childCount}
+                      onChange={(count) => setChildCount(count)}
+                    />
+                    <CountSelector
+                      title="유아"
+                      description="2세 미만"
+                      initialCount={infantCount}
+                      onChange={(count) => setInfantCount(count)}
                     />
                   </div>
                 </div>
