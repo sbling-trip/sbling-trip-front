877f78775fafaa21b435feef7916ce713ee748d1
chaeryun0 <chaeryun00@gmail.com>
2024-01-28 23:47:33 +0900

    feat: Axios 인터셉터 적용

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,4 +8,19 @@ const axiosInstance = axios.create({
   withCredentials: true,
 })
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token')
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
 export default axiosInstance
