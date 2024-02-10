102361aa93b5916976a4ebc610fcdd531db40c2b
chaeryun0 <chaeryun00@gmail.com>
2024-02-10 23:46:03 +0900

    feat: Login Page 구조 및 전체 UI 변경

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
 import Title from '@components/shared/Title'
-import ErrorMessage from '@components/shared/ErrorMessage'
 import useAuth from '@auth/useAuth'
 
-import IconKakao from '@assets/icon/icon-kakao-fill.svg?react'
-import IconNaver from '@assets/icon/icon-naver.svg?react'
 import IconGoogle from '@assets/icon/icon-google.svg?react'
+import IconKakao from '@assets/icon/icon-kakao.svg?react'
+import IconNaver from '@assets/icon/icon-naver.svg?react'
 
 import classNames from 'classnames/bind'
 import styles from './LoginPage.module.scss'
@@ -14,97 +11,32 @@ import styles from './LoginPage.module.scss'
 const cx = classNames.bind(styles)
 
 const LoginPage = () => {
-  const initialFormState = {
-    email: '',
-    password: '',
-  }
-
-  const [formData, setFormData] = useState(initialFormState)
-  const [error, setError] = useState<string | null>(null)
-
-  const { handleGoogleLogin, user } = useAuth()
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (user) {
-      navigate('/')
-    }
-  }, [user, navigate])
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    console.log(name, value)
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-
-    setError(null)
-  }
+  const { handleGoogleLogin } = useAuth()
 
   return (
     <div className={cx('loginContainer')}>
       <div className={cx('loginFormContainer')}>
         <Title title="LOGIN" subTitle="로그인" className={cx('formTitle')} />
-        <form role="form" className={cx('form')}>
-          <div className={cx('formBlock')}>
-            <label htmlFor="email">이메일</label>
-            <input
-              onChange={handleChange}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="ID@example.com"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className={cx('formBlock')}>
-            <label htmlFor="password">비밀번호</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              name="password"
-              id="password"
-              placeholder="비밀번호"
-              autoComplete="off"
-              required
-            />
-          </div>
-          {error && <ErrorMessage error={error} className={cx('error')} />}
-          <div className={cx('formBlock', 'btnBlock')}>
-            <button type="submit" className={cx('submitBtn')}>
-              로그인
+        <div className={cx('snsLoginContainer')}>
+          <div className={cx('snsLogin')}>
+            <button
+              type="button"
+              onClick={handleGoogleLogin}
+              className={cx('btn', 'google')}
+            >
+              <IconGoogle width={28} height={28} />
+              <span>구글로 시작하기</span>
+            </button>
+            <button type="button" className={cx('btn', 'kakao')}>
+              <IconKakao width={35} height={35} />
+              <span>카카오로 시작하기</span>
+            </button>
+            <button type="button" className={cx('btn', 'naver')}>
+              <IconNaver width={35} height={35} />
+              <span>네이버로 시작하기</span>
             </button>
           </div>
-          <div className={cx('snsLoginContainer')}>
-            <strong>SNS 계정으로 로그인하기</strong>
-            <div className={cx('snsLogin')}>
-              <button type="button" className={cx('btn')}>
-                <IconKakao width={60} height={60} />
-              </button>
-              <button type="button" className={cx('btn')}>
-                <IconNaver width={60} height={60} />
-              </button>
-              <button
-                type="button"
-                onClick={handleGoogleLogin}
-                className={cx('btn', 'google')}
-              >
-                <IconGoogle width={30} height={30} />
-              </button>
-            </div>
-          </div>
-          <hr />
-          <div className={cx('formBlock', 'checkUser')}>
-            <span>아직 회원이 아니신가요?</span>
-            <Link to="/signup" className={cx('formLink')}>
-              <span>회원가입하기</span>
-            </Link>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   )
