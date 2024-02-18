a6a41176d00e90822d7b187bfaae85cb5e7f445b
chaeryun0 <chaeryun00@gmail.com>
2024-02-18 16:22:33 +0900

    feat: 특정 숙소 데이터 가져오는 API 추가

--- src/components/stayList/hooks/useStayList.ts
+++ src/components/stayList/hooks/useStayList.ts
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import apiAxios from '@api/apiAxios'
-import { Stay } from '@models/stay'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@redux/store'
 import { addStays, setCurrentStay, setStays } from '@redux/staySlice'
-import { ApiResponse } from '@models/api'
+import apiAxios from '@api/apiAxios'
+import { ListApiResponse, ItemApiResponse } from '@models/api'
+import { Stay } from '@models/stay'
 
 const useStayList = () => {
   const [currentPage, setCurrentPage] = useState(0)
@@ -12,6 +12,7 @@ const useStayList = () => {
 
   const dispatch = useDispatch()
   const { stays } = useSelector((state: RootState) => state.stay)
+  const { currentStay } = useSelector((state: RootState) => state.stay)
 
   const formatLocation = (address: string) => {
     const [province, city] = address.split(' ', 2)
@@ -30,10 +31,10 @@ const useStayList = () => {
       formattedAddress: formatLocation(stay.address),
     }))
 
-  const fetchStays = async (pageNum: number) => {
+  const fetchStayList = async (pageNum: number) => {
     try {
-      const { data } = await apiAxios.get<ApiResponse<Stay>>(
-        `stay/list?cursor=${pageNum}`,
+      const { data } = await apiAxios.get<ListApiResponse<Stay>>(
+        `/stay/list?cursor=${pageNum}`,
       )
 
       if (pageNum === 0) {
@@ -44,30 +45,26 @@ const useStayList = () => {
 
       setShowMoreButton(data.result.length > 0)
     } catch (error) {
-      console.error('Error fetching stays:', error)
+      console.error('Error fetching stay list:', error)
     }
   }
 
-  const getStayBySeq = (staySeq: number) =>
-    stays.find((stay) => stay.staySeq === staySeq)
-
-  const fetchStayDetail = async (staySeq: number) => {
-    const existingStay = getStayBySeq(staySeq)
-
-    if (existingStay) {
-      dispatch(setCurrentStay(existingStay))
+  const fetchCurrentStay = async (staySeq: number) => {
+    try {
+      const { data } = await apiAxios.get<ItemApiResponse<Stay>>(
+        `/stay/info?staySeq=${staySeq}`,
+      )
+      dispatch(setCurrentStay(data.result))
+    } catch (error) {
+      console.error('Error fetching stay:', error)
     }
   }
-
-  useEffect(() => {
-    fetchStays(currentPage)
-  }, [])
 
   const loadMore = async () => {
     try {
       const nextPage = currentPage + 1
-      const { data } = await apiAxios.get<ApiResponse<Stay>>(
-        `stay/list?cursor=${nextPage}`,
+      const { data } = await apiAxios.get<ListApiResponse<Stay>>(
+        `/stay/list?cursor=${nextPage}`,
       )
 
       dispatch(addStays(formatStays(data.result)))
@@ -78,11 +75,16 @@ const useStayList = () => {
     }
   }
 
+  useEffect(() => {
+    fetchStayList(currentPage)
+  }, [])
+
   return {
     stays,
+    currentStay,
+    fetchCurrentStay,
     loadMore,
     showMoreButton,
-    fetchStayDetail,
   }
 }
 
