cc925e2e47889045bf32c6da19712563c20f7b33
chaeryun0 <chaeryun00@gmail.com>
2024-02-24 21:40:07 +0900

    feat: IconButton 컴포넌트 구현

---
+++ src/components/shared/IconButton.tsx
@@ -0,0 +1,23 @@
+import styles from './IconButton.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
+const IconButton = ({
+  label,
+  iconComponent: IconComponent,
+  onClick,
+}: {
+  label: string
+  iconComponent: React.ElementType
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}) => {
+  return (
+    <button type="button" onClick={onClick} className={cx('btn')}>
+      <IconComponent width={35} height={35} />
+      <small className={cx('label')}>{label}</small>
+    </button>
+  )
+}
+
+export default IconButton
