56b6999d197a6a40d525220d913d69dc08e47960
chaeryun0 <chaeryun00@gmail.com>
2024-03-01 00:01:36 +0900

    feat: Review 수정 컴포넌트 구현

---
+++ src/components/stay/review/ReviewEdit.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import Dimmed from '@components/shared/Dimmed'
+import { Review } from '@models/review'
+
+import IconClose from '@assets/icon/icon-close.svg?react'
+import IconStar from '@assets/icon/icon-star.svg?react'
+import classNames from 'classnames/bind'
+import styles from './ReviewEdit.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface ReviewEditProps {
+  review: Review
+}
+
+const ReviewEdit = ({ review }: ReviewEditProps) => {
+  const [editedReview, setEditedReview] = useState({ ...review })
+  const [selectedStars, setSelectedStars] = useState(review.reviewScore - 1)
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedReview({ ...editedReview, reviewTitle: e.target.value })
+  }
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditedReview({ ...editedReview, reviewContent: e.target.value })
+  }
+
+  const handleStarClick = (index: number) => {
+    setSelectedStars(index)
+    setEditedReview({ ...editedReview, reviewScore: index + 1 })
+  }
+
+  return (
+    <Dimmed>
+      <div className={cx('reviewEditContainer')}>
+        <div className={cx('contents')}>
+          <div className={cx('top')}>
+            <div className={cx('starWrap')}>
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  className={cx('selectStar')}
+                  onClick={() => handleStarClick(index)}
+                >
+                  <IconStar
+                    width={45}
+                    height={45}
+                    fill={`var(--${
+                      selectedStars >= index ? 'yellow' : 'gray'
+                    }300)`}
+                  />
+                </span>
+              ))}
+            </div>
+            <div className={cx('starText')}>
+              <span>별점을 선택해주세요.</span>
+            </div>
+          </div>
+          <div className={cx('bottom')}>
+            <div className={cx('editor')}>
+              <input
+                type="text"
+                value={editedReview.reviewTitle}
+                onChange={handleTitleChange}
+                className={cx('reviewInput')}
+              />
+              <textarea
+                value={editedReview.reviewContent}
+                onChange={handleContentChange}
+              />
+            </div>
+            <div className={cx('btnWrap')}>
+              <button className={cx('cancelBtn')}>취소</button>
+              <button type="submit" className={cx('submitBtn')}>
+                저장하기
+              </button>
+            </div>
+          </div>
+          <button type="button" className={cx('closeBtn')}>
+            <IconClose width={20} height={20} />
+          </button>
+        </div>
+      </div>
+    </Dimmed>
+  )
+}
+
+export default ReviewEdit
