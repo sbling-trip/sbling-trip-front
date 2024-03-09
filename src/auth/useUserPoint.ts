2bc6ae63a2227d2addfbc1dcbfe51f9c1bd0e684
chaeryun0 <chaeryun00@gmail.com>
2024-03-09 18:07:01 +0900

    feat: 유저 포인트 관련 처리를 위한 커스텀 훅 구현

---
+++ src/auth/useUserPoint.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { RootState } from '@redux/store'
+import { setPoints } from '@redux/pointSlice'
+import apiAxios from '@api/apiAxios'
+import { ItemApiResponse } from '@models/api'
+import { Point } from '@models/point'
+
+const useUserPoint = () => {
+  const dispatch = useDispatch()
+  const { user } = useSelector((state: RootState) => state.user)
+  const { points } = useSelector((state: RootState) => state.point)
+
+  const fetchUserPoint = async () => {
+    try {
+      const { data } = await apiAxios.get<ItemApiResponse<Point>>('/point/me')
+      dispatch(setPoints(data.result))
+    } catch (error) {
+      console.error('Failed to fetch user point:', error)
+    }
+  }
+
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem('access_token')
+
+    const fetchUserPointData = async () => {
+      if (storedAccessToken) {
+        fetchUserPoint()
+      }
+    }
+
+    if (user) {
+      fetchUserPointData()
+    }
+  }, [])
+
+  return { points, fetchUserPoint }
+}
+
+export default useUserPoint
