2d5f95b5df0bfd3cad2eb4a173adf4ee115385d8
chaeryun0 <chaeryun00@gmail.com>
2024-03-09 20:06:30 +0900

    feat: 예약자 정보 컴포넌트로 구현

---
+++ src/components/reservation/UserInfo.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Title from '@components/shared/Title'
+import ErrorMessage from '@components/shared/ErrorMessage'
+
+import classNames from 'classnames/bind'
+import styles from './UserInfo.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface UserInfoProps {
+  formData: {
+    name: string
+    phone: string
+    errors: { name: string; phone: string }
+    isNameValid: boolean
+    isPhoneValid: boolean
+  }
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    fieldName: string,
+  ) => void
+}
+
+const UserInfo = React.memo(
+  ({ formData, handleInputChange }: UserInfoProps) => {
+    const { name, phone, errors } = formData
+
+    return (
+      <section className={cx('sectionContainer')}>
+        <Title title="예약자 정보" subTitle="" className={cx('sectionTitle')} />
+        <div className={cx('reservationPerson')}>
+          <div className={cx('rowBlock')}>
+            <label htmlFor="name">예약자 이름</label>
+            <div className={cx('inputWrap')}>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="예약자"
+                autoComplete="off"
+                value={name}
+                onChange={(e) => handleInputChange(e, 'name')}
+                required
+              />
+            </div>
+            <ErrorMessage error={errors.name} className={cx('errorMsg')} />
+          </div>
+          <div className={cx('rowBlock')}>
+            <label htmlFor="phone">휴대폰 번호</label>
+            <div className={cx('inputWrap')}>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                inputMode="tel"
+                placeholder="010-1234-5678"
+                autoComplete="off"
+                value={phone}
+                onChange={(e) => handleInputChange(e, 'phone')}
+                required
+              />
+            </div>
+            <ErrorMessage error={errors.phone} className={cx('errorMsg')} />
+          </div>
+        </div>
+      </section>
+    )
+  },
+)
+
+UserInfo.displayName = 'UserInfo'
+
+export default UserInfo
