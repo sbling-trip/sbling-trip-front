fc7dbe939656dfe32c3bd17b85992b9e5b7e316a
chaeryun0 <chaeryun00@gmail.com>
2024-03-09 18:33:41 +0900

    feat: StayList 컴포넌트 수정

--- src/components/stayList/StayList.tsx
+++ src/components/stayList/StayList.tsx
@@ -1,16 +1,21 @@
 import { useRef } from 'react'
-import useStayList from '@components/stayList/hooks/useStayList'
 import StayItem from '@components/stayList/StayItem'
 import Title from '@components/shared/Title'
 import ObserverPlaceholder from '@components/shared/ObserverPlaceholder'
+import { Stay } from '@models/stay'
 
 import classNames from 'classnames/bind'
 import styles from './StayList.module.scss'
 
 const cx = classNames.bind(styles)
 
-const StayList = () => {
-  const { stays, loadMore, toggleWish } = useStayList()
+interface StayListProps {
+  stays: Stay[]
+  fetchLoadMore: () => void
+  toggleWish: (staySeq: number, wishState: boolean) => void
+}
+
+const StayList = ({ stays, fetchLoadMore, toggleWish }: StayListProps) => {
   const observerRef = useRef<HTMLLIElement | null>(null)
 
   if (!stays) {
@@ -19,18 +24,14 @@ const StayList = () => {
 
   return (
     <div className={cx('stayListContainer')}>
-      <Title
-        title="실시간 베스트"
-        subTitle=""
-        className={cx('stayListTitle')}
-      />
+      <Title title="전체 숙소" subTitle="" className={cx('stayListTitle')} />
       <ul className={cx('stayItemWrap')}>
         {stays.map((stay, index) => [
           <StayItem stay={stay} key={stay.staySeq} toggleWish={toggleWish} />,
           index === stays.length - 1 && (
             <ObserverPlaceholder
               observerRef={observerRef}
-              onLoadMore={loadMore}
+              onLoadMore={fetchLoadMore}
               key={`observer-${index}`}
             />
           ),
