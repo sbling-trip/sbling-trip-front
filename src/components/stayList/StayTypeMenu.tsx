3646d725ce25b2dd8cf17ac070932c07fae1441c
chaeryun0 <chaeryun00@gmail.com>
2024-03-18 01:28:44 +0900

    refactor: MenuItem을 별도 컴포넌트로 분리

--- src/components/stayList/StayTypeMenu.tsx
+++ src/components/stayList/StayTypeMenu.tsx
@@ -1,4 +1,5 @@
 import { StayType } from '@models/stay'
+
 import classNames from 'classnames/bind'
 import styles from './StayTypeMenu.module.scss'
 
@@ -10,27 +11,56 @@ interface StayTypeMenuProps {
 }
 
 const StayTypeMenu = ({ activeTab, handleTabClick }: StayTypeMenuProps) => {
-  const renderMenu = (type: StayType, label: string) => (
+  return (
+    <div className={cx('stayTypeMenu')}>
+      <ul role="tablist" className={cx('tablist')}>
+        <MenuItem
+          type={StayType.Hotel}
+          label="호텔"
+          active={activeTab === StayType.Hotel}
+          handleTabClick={handleTabClick}
+        />
+        <MenuItem
+          type={StayType.Motel}
+          label="모텔"
+          active={activeTab === StayType.Motel}
+          handleTabClick={handleTabClick}
+        />
+        <MenuItem
+          type={StayType.Pension}
+          label="펜션"
+          active={activeTab === StayType.Pension}
+          handleTabClick={handleTabClick}
+        />
+        <MenuItem
+          type={StayType.GuestHouse}
+          label="게스트하우스"
+          active={activeTab === StayType.GuestHouse}
+          handleTabClick={handleTabClick}
+        />
+      </ul>
+    </div>
+  )
+}
+
+interface MenuItemProps {
+  type: StayType
+  label: string
+  active: boolean
+  handleTabClick: (type: StayType) => void
+}
+
+const MenuItem = ({ type, label, active, handleTabClick }: MenuItemProps) => {
+  return (
     <li
       role="tab"
       tabIndex={0}
-      aria-selected={activeTab === type ? 'true' : 'false'}
-      className={activeTab === type ? cx('active') : ''}
+      aria-selected={active ? 'true' : 'false'}
+      className={active ? cx('active') : ''}
       onClick={() => handleTabClick(type)}
     >
       {label}
     </li>
-  )
-
-  return (
-    <div className={cx('stayTypeMenu')}>
-      <ul role="tablist" className={cx('tablist')}>
-        {renderMenu(StayType.Hotel, '호텔')}
-        {renderMenu(StayType.Motel, '모텔')}
-        {renderMenu(StayType.Pension, '펜션')}
-        {renderMenu(StayType.GuestHouse, '게스트하우스')}
-      </ul>
-    </div>
   )
 }
 
