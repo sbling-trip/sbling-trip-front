fd6c00f60dd8a863da8eb9a1d945f84ccde4c0d9
chaeryun0 <chaeryun00@gmail.com>
2024-03-22 23:19:30 +0900

    feat: 메인 배너 캐러셀 추가 및 스타일링

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,15 +3,22 @@ import StayList from '@components/stayList/StayList'
 import StayTypeMenu from '@components/stayList/StayTypeMenu'
 import StayItemContents from '@components/stayList/StayItemContents'
 import Title from '@components/shared/Title'
+import Carousel from '@components/shared/Carousel'
 import CustomCarousel from '@components/shared/CustomCarousel'
 
 import useStayList from '@components/stayList/hooks/useStayList'
 import { StayType } from '@models/stay'
 
+import banner1 from '@assets/banner_1.png'
+import banner2 from '@assets/banner_2.png'
+import banner3 from '@assets/banner_3.png'
+import banner4 from '@assets/banner_5.png'
 import classNames from 'classnames/bind'
 import styles from './MainPage.module.scss'
 
 const cx = classNames.bind(styles)
+
+const MAIN_BANNER_IMAGES = [banner1, banner2, banner3, banner4]
 
 const MainPage = () => {
   const {
@@ -54,6 +61,26 @@ const MainPage = () => {
               staySeq: stay.staySeq,
               contents: <StayItemContents stay={stay} />,
             }))}
+          />
+        </div>
+      </section>
+      <section className={cx('mainBanner')}>
+        <div className={cx('inner')}>
+          <Carousel
+            images={MAIN_BANNER_IMAGES}
+            navigation={false}
+            autoplay={true}
+            autoplayDelay={3500}
+            effect="fade"
+            fadeEffect={{
+              crossFade: true,
+              duration: 800,
+            }}
+            pagination={{
+              type: 'bullets',
+              clickable: true,
+            }}
+            className={cx('carousel')}
           />
         </div>
       </section>
