db0ac6f8104ad0978b708e1d38107a7e6c8e239f
chaeryun0 <chaeryun00@gmail.com>
2024-03-22 23:55:45 +0900

    feat: useAuth 훅에 사용자 정보 관련 로직 통합

--- src/auth/useAuth.ts
+++ src/auth/useAuth.ts
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { useAlertContext } from '@hooks/useAlertContext'
-import useUserInfo from './useUserInfo'
 
 import authClientAxios from '@api/authClientAxios'
+import apiClientAxios from '@api/apiClientAxios'
+import { RootState } from '@redux/store'
 import { resetUser, setUser } from '@redux/userSlice'
 import { User } from '@models/user'
+import { ItemApiResponse } from '@models/api'
 
 export enum LOGIN_STATUS {
   LoginSuccess = 0,
@@ -21,7 +23,7 @@ const useAuth = () => {
   const { openAlert } = useAlertContext()
 
   const dispatch = useDispatch()
-  const { user } = useUserInfo()
+  const { user } = useSelector((state: RootState) => state.user)
 
   const handleGoogleLogin = () => {
     window.location.href = `${
@@ -140,14 +142,71 @@ const useAuth = () => {
     }, 100)
   }
 
+  const fetchValidateToken = async () => {
+    try {
+      await authClientAxios.get('/tokens/info')
+      return true
+    } catch (error) {
+      console.error('Error validating token:', error)
+      localStorage.removeItem('access_token')
+      openAlert({
+        title: '토큰이 유효하지 않습니다. 다시 로그인 해주세요.',
+        onConfirmClick: handleConfirmClick,
+      })
+      return false
+    }
+  }
+
+  const fetchUserInfo = async () => {
+    try {
+      const { data } =
+        await apiClientAxios.get<ItemApiResponse<User>>('/user/me')
+      dispatch(setUser(data.result))
+    } catch (error) {
+      console.error('Failed to fetch user data:', error)
+    }
+  }
+
+  const fetchUpdateUserInfo = async (updatedUserData: Partial<User>) => {
+    try {
+      const { data } = await apiClientAxios.put<ItemApiResponse<User>>(
+        '/user/update',
+        updatedUserData,
+      )
+      dispatch(setUser(data.result))
+      await fetchUserInfo()
+    } catch (error) {
+      console.error('Failed to update user data:', error)
+    }
+  }
+
   useEffect(() => {
     if (!user) {
       dispatch(resetUser())
     }
   }, [])
 
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem('access_token')
+
+    const fetchUserData = async () => {
+      if (storedAccessToken) {
+        const isValidToken = await fetchValidateToken()
+        if (isValidToken) {
+          fetchUserInfo()
+        }
+      }
+    }
+
+    if (!user) {
+      fetchUserData()
+    }
+  }, [])
+
   return {
     user,
+    fetchUserInfo,
+    fetchUpdateUserInfo,
     fetchSignOut,
     handleSignOut,
     handleGoogleLogin,
