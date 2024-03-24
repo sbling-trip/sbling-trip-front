84480a622c748dda20cb85d7e2e6efa17021ca9e
chaeryun0 <chaeryun00@gmail.com>
2024-03-24 23:13:03 +0900

    refactor: 개발 진행 알림창 메시지 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import Title from '@components/shared/Title'
 import useAuth from '@auth/useAuth'
+import { useAlertContext } from '@hooks/useAlertContext'
 
 import IconGoogle from '@assets/icon/icon-google.svg?react'
 import IconKakao from '@assets/icon/icon-kakao.svg?react'
@@ -11,7 +12,16 @@ import styles from './LoginPage.module.scss'
 const cx = classNames.bind(styles)
 
 const LoginPage = () => {
+  const { openAlert } = useAlertContext()
   const { handleGoogleLogin } = useAuth()
+
+  const handleUnderDevelopment = () => {
+    openAlert({
+      title: '아직 개발 진행 중입니다 !',
+      subTitle: ' 구글 계정으로 로그인해주세요. 🙏🏻',
+      onConfirmClick: () => {},
+    })
+  }
 
   return (
     <div className={cx('loginContainer')}>
@@ -27,11 +37,19 @@ const LoginPage = () => {
               <IconGoogle width={28} height={28} />
               <span>구글로 시작하기</span>
             </button>
-            <button type="button" className={cx('btn', 'kakao')}>
+            <button
+              type="button"
+              className={cx('btn', 'kakao')}
+              onClick={handleUnderDevelopment}
+            >
               <IconKakao width={35} height={35} />
               <span>카카오로 시작하기</span>
             </button>
-            <button type="button" className={cx('btn', 'naver')}>
+            <button
+              type="button"
+              className={cx('btn', 'naver')}
+              onClick={handleUnderDevelopment}
+            >
               <IconNaver width={35} height={35} />
               <span>네이버로 시작하기</span>
             </button>
