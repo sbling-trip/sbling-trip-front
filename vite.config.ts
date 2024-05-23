31797b2f5b7485d66248f36d5757bcae30f359b2
chaeryun0 <chaeryun00@gmail.com>
2024-05-23 18:51:23 +0900

    feat: 이미지 최적화를 위한 imagemin 플러그인 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    viteImagemin({
+      optipng: {
+        optimizationLevel: 7, // PNG 최적화 수준 설정 (0-7)
+      },
+      pngquant: {
+        quality: [0.8, 0.9], // PNG 품질 설정 (0-1)
+        speed: 4, // PNG 압축 속도 설정 (1-11)
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox', // 뷰박스 제거 플러그인 활성화
+          },
+          {
+            name: 'removeEmptyAttrs', // 빈 속성 제거 플러그인 비활성화
+            active: false,
+          },
+        ],
+      },
+      webp: { quality: 75 }, // WebP 품질 설정 (0-100)
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
